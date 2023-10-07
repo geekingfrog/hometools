58857326b55ade0aff7e3720c8ec346f7e7958af
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2023-10-07 11:03:48 +0100

    refactor session, use tower_session

--- src/server.rs
+++ src/server.rs
@@ -1,17 +1,18 @@
 use askama::Template;
 use axum::{
+    async_trait,
+    error_handling::HandleErrorLayer,
+    extract::FromRequestParts,
+    http::{request::Parts, StatusCode},
     response::{Html, IntoResponse},
     routing::{self, get, post},
-    Form, Router,
+    BoxError, Form, Router,
 };
-use axum_sessions::{
-    async_session,
-    extractors::{ReadableSession, WritableSession},
-    SessionLayer,
-};
-use serde::Deserialize;
+
+use serde::{Deserialize, Serialize};
 use tower::ServiceBuilder;
 use tower_http::{services::ServeDir, trace::TraceLayer};
+use tower_sessions::{Session, SessionManagerLayer};
 
 pub struct Server {
     router: Router<()>,
@@ -21,18 +22,64 @@ pub struct Server {
 #[template(path = "index.html")]
 struct IndexTemplate;
 
+#[derive(Template)]
+#[template(path = "tools.html")]
+struct ToolsTemplate;
+
+#[derive(Template)]
+#[template(path = "not-found.html")]
+struct NotFoundTemplate;
+
 #[derive(Debug, Deserialize)]
 struct LoginForm {
     username: String,
     password: String,
 }
 
-async fn login_post(
-    mut sess: WritableSession,
+#[derive(Debug, Deserialize, Serialize)]
+struct AuthedUser {
+    username: String,
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for AuthedUser
+where
+    S: Send + Sync,
+{
+    type Rejection = StatusCode;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        use axum::RequestPartsExt;
+        tracing::info!("COUCOU extracting here!");
+
+        let sess = parts.extract::<Session>().await.expect("got the session");
+
+        tracing::info!("COUCOU got session!");
+        let user = sess
+            .get("user")
+            .unwrap_or(None)
+            .ok_or(StatusCode::UNAUTHORIZED)?;
+        tracing::info!("COUCOU got user! {:?}", sess);
+
+        Ok(user)
+    }
+}
+
+async fn root_post(
+    mut sess: Session,
+    stuff: Option<AuthedUser>,
     Form(login): Form<LoginForm>,
 ) -> axum::response::Response {
-    tracing::info!("stuff we got: {login:?}");
-    sess.insert_raw("username", format!("{}-{}", login.username, login.password));
+    // tracing::debug!("got stuff from session: {:?}", sess.get_raw("username"));
+    // tracing::debug!("authed user? {:?}", stuff);
+    // sess.insert_raw("username", format!("{}-{}", login.username, login.password));
+    sess.insert(
+        "user",
+        AuthedUser {
+            username: login.username,
+        },
+    )
+    .unwrap();
 
     axum::response::Redirect::to("/").into_response()
 }
@@ -40,23 +87,34 @@ async fn login_post(
 impl Server {
     pub fn new() -> Self {
         let trace_layer = ServiceBuilder::new().layer(TraceLayer::new_for_http());
-        let store = async_session::MemoryStore::new();
-        let secret = b"c20d16e16b32c77f63bc42d7565a75026211cb67f34d77553091761cba16943bbb51c1bcbd8fc08ee23e010407e49fa5f5b516f0625d247bc436c3938debf71c";
-        let secret = hex::decode(secret).unwrap();
-
-        let session_layer = SessionLayer::new(store, &secret);
+        let session_store = tower_sessions::MemoryStore::default();
+        let session_service = ServiceBuilder::new()
+            .layer(HandleErrorLayer::new(|_: BoxError| async {
+                StatusCode::BAD_REQUEST
+            }))
+            .layer(SessionManagerLayer::new(session_store));
 
         let app = Router::new()
             .layer(trace_layer)
-            .layer(session_layer)
             .route(
                 "/",
                 get(|| async {
                     let tpl = IndexTemplate {};
                     Html(tpl.render().unwrap())
-                }),
+                })
+                .post(root_post),
             )
-            .route("/login", post(login_post))
+            .route(
+                "/tools",
+                get(|| async { Html(ToolsTemplate {}.render().unwrap()) }),
+            )
+            .layer(session_service)
+            .fallback(|| async {
+                (
+                    StatusCode::NOT_FOUND,
+                    Html(NotFoundTemplate {}.render().unwrap()),
+                )
+            })
             .nest_service("/static", routing::get_service(ServeDir::new("static")));
 
         Server { router: app }
